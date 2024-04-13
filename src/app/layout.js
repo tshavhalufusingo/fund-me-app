ffe6d40309819d7f8bda3ccbb2607b526270c2a2
tshavhalufusingo <2274807@students.wits.ac.za>
2024-04-13 19:48:12 +0200

    Init Commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <header>
+            <h1>Fund ME</h1>
+          </header>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import styles from "./page.module.css";
-import "./styles.css";
+import styles from "../page.module.css";
+import "../styles.css";
 import Link from "next/link";
 
 export default function Home() {
@@ -9,11 +9,12 @@ export default function Home() {
       <form>
         <input type="email" placeholder="User email"></input>
         <input type="password" placeholder="User password"></input>
-        <button type="submit">Login</button>
+        <input type="password" placeholder="Re-enter User password"></input>
+        <button type="submit">Register</button>
       </form>
-      <p>If you have no account click register below</p>
+      <p>If you already have an account click login below</p>
       <button id="register" type="submit">
-        <Link href="/register">Register</Link>
+        <Link href="/">Login</Link>
       </button>
     </main>
   );
