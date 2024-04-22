970388d99bcc6b6dea52560a7a469e8b1f44a8b8
clearence <2440504@students.wits.ac.za>
2024-04-22 21:37:25 +0000

    update on test

--- src/app/register/__test__/register.test.jsx
+++ src/app/register/__test__/register.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, getByTestId } from "@testing-library/react";
 import { act } from "@testing-library/react"; // for handling async operations
 import Home from '../page'
 
@@ -17,6 +17,57 @@ jest.mock('next/navigation', () => ({
 
 describe('Home Component', () => {
 
+  it('should display error message on short password', async () => {
+    render(<Home />);
+  
+    const password = screen.getByPlaceholderText('Password');
+    const confirmPassword = screen.getByPlaceholderText('Confirm password');
+    const submitButton = screen.getByText('Register');
+  
+    fireEvent.change(password, { target: { value: 'short' } });
+    fireEvent.change(confirmPassword, { target: { value: 'short' } });
+  
+    await act(async () => fireEvent.click(submitButton));
+  
+    
+  });
+  
+
+  it('should display error message on missing fields', async () => {
+    render(<Home />);
+  
+    const submitButton = screen.getByText('Register');
+  
+    await act(async () => fireEvent.click(submitButton));
+  
+    
+  });
+  
+
+  it('should submit the form with valid input', async () => {
+    render(<Home />);
+  
+    const firstName = screen.getByPlaceholderText('User firstname');
+    const lastName = screen.getByPlaceholderText('User lastname');
+    const email = screen.getByPlaceholderText('User email');
+    const password = screen.getByPlaceholderText('Password');
+    const confirmPassword = screen.getByPlaceholderText('Confirm password');
+    const submitButton = screen.getByText('Register');
+  
+    fireEvent.change(firstName, { target: { value: 'John' } });
+    fireEvent.change(lastName, { target: { value: 'Doe' } });
+    fireEvent.change(email, { target: { value: 'test@example.com' } });
+    fireEvent.change(password, { target: { value: 'testpassword' } });
+    fireEvent.change(confirmPassword, { target: { value: 'testpassword' } });
+  
+    await act(async () => fireEvent.click(submitButton));
+  
+    
+  });
+  
+
+ 
+
   it('should render registration form', () => {
     render(<Home />);
 
@@ -28,7 +79,7 @@ describe('Home Component', () => {
     expect(screen.getByPlaceholderText('Confirm password')).toBeInTheDocument();
     expect(screen.getByText('Register')).toBeInTheDocument();
   });
-
+// You can assert here that an error message is displayed for short password
   it('should log error message on missing first name', async () => {
     render(<Home />);
 
