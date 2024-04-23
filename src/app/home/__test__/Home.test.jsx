3b92e32e50c3beb42140d9bf7016d51a7636bea9
tshavhalufusingo <2274807@students.wits.ac.za>
2024-04-23 15:44:31 +0200

    update test

--- src/app/home/__test__/Home.test.jsx
+++ src/app/home/__test__/Home.test.jsx
@@ -1,41 +1,40 @@
 import { render } from "@testing-library/react";
-import Page from '../page'
+import Page from "../page";
 
 // Mock next-auth/react to simulate user data
-jest.mock('next-auth/react', () => ({
+jest.mock("next-auth/react", () => ({
   useSession: () => ({
     data: {
       user: {
-        firstName: 'John',
-        lastName: 'Doe',
-        role: 'Applicant',
+        firstName: "John",
+        lastName: "Doe",
+        role: "Applicant",
       },
     },
-    status: 'authenticated', // Adjust as needed (loading, idle)
+    status: "authenticated", // Adjust as needed (loading, idle)
   }),
 }));
 
-describe('Page Component', () => {
-  it('should render user information for logged-in user', () => {
+describe("Page Component", () => {
+  it("should render user information for logged-in user", () => {
     const { getByText } = render(<Page />);
 
-    const expectedText = 'I am Doe John with a role of Applicant';
+    const expectedText = "I am Doe John with a role of Applicant";
     expect(getByText(expectedText)).toBeInTheDocument();
   });
+  //   it('should not render user role for non-logged-in user', () => {
+  //     // Mock empty user data for testing
+  //     jest.mock('next-auth/react', () => ({
+  //       useSession: () => ({
+  //         data: {
+  //           user: null,
+  //         },
+  //         status: 'authenticated', // Adjust as needed (loading, idle)
+  //       }),
+  //     }));
 
-//   it('should not render user role for non-logged-in user', () => {
-//     // Mock empty user data for testing
-//     jest.mock('next-auth/react', () => ({
-//       useSession: () => ({
-//         data: {
-//           user: null,
-//         },
-//         status: 'authenticated', // Adjust as needed (loading, idle)
-//       }),
-//     }));
+  //     const { queryByText } = render(<Page />);
 
-//     const { queryByText } = render(<Page />);
-
-//     expect(queryByText(/role/i)).not.toBeInTheDocument(); // Using regular expression for "role" (case-insensitive)
-//   });
+  //     expect(queryByText(/role/i)).not.toBeInTheDocument(); // Using regular expression for "role" (case-insensitive)
+  //   });
 });
