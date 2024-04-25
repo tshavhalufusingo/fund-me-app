2316fb5a08baecaeda85e7f2bbb36710c43ef86d
tshavhalufusingo <2274807@students.wits.ac.za>
2024-04-25 16:53:31 +0200

    display users info

--- src/app/components/adminPages/userWaintingApproval.js
+++ src/app/components/adminPages/userWaintingApproval.js
@@ -1,7 +1,19 @@
-'use client'
-
+"use "
+import { useState, useEffect } from 'react'
 export default function UserWaintingApproval() {
+
+    const [data, setData] = useState(null)
+    
+   
+    useEffect(() => {
+      fetch('/api/users')
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data)
+        })
+    }, [])
+
   return (
-    <div>userWaintingApproval</div>
+    <div> <p>{data?.map((userdata) => {return <p key={userdata.userId}>{userdata.userEmail}</p> } )} </p> </div>
   )
 }
