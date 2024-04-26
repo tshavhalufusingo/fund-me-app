01386b05c4ed5779f79599e7608d05ab104ca6d6
tshavhalufusingo <2274807@students.wits.ac.za>
2024-04-26 13:39:40 +0200

    adding user  table

--- src/app/components/adminPages/userWaintingApproval.js
+++ src/app/components/adminPages/userWaintingApproval.js
@@ -1,19 +1,44 @@
-"use "
-import { useState, useEffect } from 'react'
-export default function UserWaintingApproval() {
+"use client";
 
-    const [data, setData] = useState(null)
-    
-   
-    useEffect(() => {
-      fetch('/api/users')
-        .then((res) => res.json())
-        .then((data) => {
-          setData(data)
-        })
-    }, [])
+import { useState, useEffect } from "react";
+export default function UserWaintingApproval() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
 
   return (
-    <div> <p>{data?.map((userdata) => {return <p key={userdata.userId}>{userdata.userEmail}</p> } )} </p> </div>
-  )
+    <div>
+      <header>
+        <h2>
+          Users awaitimg approval
+        </h2>
+      </header>
+      <table>
+        <tr>
+          <th>User id</th>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Email</th>
+        </tr>
+      </table>
+      <p>
+        {data?.map((userdata) => {
+          return (
+            <tr key={userdata.userId}>
+              <td>{userdata.userId}</td>
+              <td>{userdata.firstname}</td>
+              <td>{userdata.lastname}</td>
+              <td>{userdata.userEmail}</td>
+            </tr>
+          );
+        })}{" "}
+      </p>{" "}
+    </div>
+  );
 }
