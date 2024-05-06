47c037723f1b33de7bd28e8408f8b5f362e41a76
Teboho02 <86244195+Teboho02@users.noreply.github.com>
2024-05-06 09:31:57 +0200

    make aplication

--- src/app/components/applicantPages/applicationForm/page.js
+++ src/app/components/applicantPages/applicationForm/page.js
@@ -1,62 +1,87 @@
-'use client'
+"use client";
+import styles from "../../../page.module.css";
+import "../../../styles.css";
 import { useSession } from "next-auth/react";
-import styles from "./../../../page.module.css";
-import { useRouter } from "next/router";
-import "../../../styles.css"
+import { useParams, } from "next/navigation";
 
-export default function Home ( {postId }) {
-  //const router = useRouter();
+export default function ApplicationForm() {
+  const params = useParams();
+  const { data: session, status } = useSession();
+  const attachmentUrl = ""; // Add URL to the uploaded file here
 
-const { data: session } = useSession();
-const handleRegister = async (event) => {
+  const handlefile =(event)=>{
 
-   
-        console.log(postId);
-        console.log(postId);
-        console.log(postId);
-        console.log(postId);
-        console.log(postId);
+    const fileReader = new FileReader()
+    const file =  event.target.files[0];
 
-
-    
-    event.preventDefault();
-    const name = document.getElementById("Name").value;
-    const phoneNum = document.getElementById("PhoneNum").value;
-    const motiv = document.getElementById("Motivation").value;
-    try {
-      const resp = await fetch("/api/applicationForm", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, phoneNum , motiv,}),
-      });
-
-      if (resp.ok) {
-        // Handle success
-      } else {
-        console.error("Error:", resp.statusText);
-        // Handle error
-      }
-    } catch (error) {
-      console.error("Error:", error.message);
-      // Handle network error
+    if(!file){
+      return;
     }
-  };
 
-  const goToLoginPage = (e) => {
-    e.preventDefault();
- //   router.push("/");
+    const allowedExt = /\.(pdf|txt)$/i;
+
+    if(!allowedExt.test(file.name)){
+      alert("Invalid type only pdf allowed")
+      
+    }
+
+    fileReader.readAsDataURL(file);
+    fileReader.addEventListener('loadend',()=>{
+      if(fileReader.result != null){
+        console.log(fileReader.result)
+      }
+    })
+  }
+
+  const handleApplication = async (e) => {
+    e.preventDefault;
+    const inputData = {
+      postId: params.postId,
+      userId: session?.user.id,
+    };
+
+    const response = await fetch("/api/applications", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputData),
+    });
   };
 
   return (
     <main className={styles.main}>
-      <div>
-        <input id="Name" type="text" placeholder="Name" required />
-        <input id="PhoneNum" type="text" placeholder="Phone Number" required />
-        <input id="Motivation" type="text" placeholder="Motivation" required/>
-        <button onClick={handleRegister}>APPLY</button>
-      </div>
+      <form className="applicationForm">
+        <label htmlFor="name">Full name:</label>
+        <input type="text" id="name" name="name" required />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="amount">Motivation</label>
+        <input type="text" id="amount" name="amount" required />
+
+        <label htmlFor="attachment">Attachments:</label>
+        <input type="file" id="attachment" onChange={handlefile} name="attachment" required />
+
+        {attachmentUrl && (
+          <div>
+            <label>Attachment:</label>
+            <a href={attachmentUrl} download>
+              Download Attachment
+            </a>
+          </div>
+        )}
+
+        <label htmlFor="documentType">Document Type:</label>
+        <select id="documentType" name="documentType" required>
+          <option value="Identity Document">ID</option>
+          <option value="2">CV</option>
+          <option value="other">Other</option>
+        </select>
+
+        <button onClick={handleApplication}>Submit</button>
+      </form>
     </main>
   );
 }
