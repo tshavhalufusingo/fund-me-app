18b86ff1a7d31510a5a7ce1ccb76f6653a3a9118
Teboho02 <moloiteboho0209@gmail.com>
2024-05-15 14:46:55 +0200

    UI Update

--- src/app/components/profile/page.js
+++ src/app/components/profile/page.js
@@ -1,14 +1,46 @@
-
-
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 export default function getProfile(){
 
+    const session = useSession();
+
+    const myId = session?.id;
+
+
   
+
+    useEffect(() => {
+
+
+
+        async function fetchData() {
+            try {
+                //create an API To fetch a specific user's informationa another one to allow them to change some information
+              const response = await fetch("../../api/users");
+              if (!response.ok) {
+                throw new Error("Failed to fetch data");
+              }
+              const jsonData = await response.json();
+
+              
+            }catch(Exeption){
+                console.log(Exeption);
+            }
+        }
+
+
+        
+
+    }, []);
+
+
+
 
     return (
 
     <h1 className="profileheader">
-        // To implement
+
     </h1>
 
     );
