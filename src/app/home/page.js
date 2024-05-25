0ad50322c45cd30965afedd14a68b6d5eeb7b9df
clearence <2440504@students.wits.ac.za>
2024-05-25 21:24:02 +0200

    tests update

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,73 +1,73 @@
 "use client";
-
 import styles from "../page.module.css";
 import { useSession } from "next-auth/react";
 import AdminHome from "./../components/adminPages/adminHome/page";
 import FundManagerHome from "./../components/fundManagerPages/fundManagerHome/page";
 import ApplicantHome from "./../components/applicantPages/applicantHome/page";
 
+// Page component responsible for rendering different home pages based on the user's role
 export default function Page() {
-  const { data: session, status } = useSession(); // Get session data and status
+  const { data: session, status } = useSession();
 
-  if (status === "loading") {
-    // Show loading state while session data is being fetched
-    return (
-      <main className={styles.main}>
-        <p>Loading...</p>
-      </main>
-    );
-  }
-
+  // Check if user session is available
   if (session?.user) {
-    // Check if the user's account is approved
-    if (session.user.statusId !== 2) {
+
+    // Check if user account is approved
+    if (session?.user.statusId != 2) {
       return (
-        <main className={styles.main}>
-          <p>Your account is not approved</p>
-        </main>
+        <>
+          <main className={styles.main}>
+            <p>Your account is not approved</p>
+          </main>
+        </>
       );
     }
 
-    // Check if the user's account is blocked
-    if (session.user.userBlock) {
+    // Check if user account is blocked
+    if (session?.user.userBlock) {
       return (
-        <main className={styles.main}>
-          <p>Your account is blocked</p>
-          <p>Contact admin at help@fundmeapp.com</p>
-        </main>
+        <>
+          <main className={styles.main}>
+            <p>Your account is blocked</p>
+            <p>contact admin at help@fundmeapp.com</p>
+          </main>
+        </>
       );
     }
 
-    // Render home page based on the user's role
-    switch (session.user.role) {
-      case "Admin":
-        return (
+    // Render AdminHome if user role is Admin
+    if (session?.user.role == "Admin") {
+      return (
+        <>
           <main className={styles.main}>
             <AdminHome />
           </main>
-        );
-      case "FundManager":
-        return (
+        </>
+      );
+    } 
+    // Render FundManagerHome if user role is FundManager
+    else if (session?.user.role == "FundManager") {
+      return (
+        <>
           <main className={styles.main}>
             <FundManagerHome />
           </main>
-        );
-      case "Applicant":
-        return (
+        </>
+      );
+    } 
+    // Render ApplicantHome if user role is Applicant
+    else if (session?.user.role == "Applicant") {
+      return (
+        <>
           <main className={styles.main}>
             <ApplicantHome />
           </main>
-        );
-      default:
-        return (
-          <main className={styles.main}>
-            <p>Unknown role</p>
-          </main>
-        );
+        </>
+      );
     }
   }
 
-  // If no session is found, prompt the user to log in
+  // Default case when no session is found or user is not logged in
   return (
     <main className={styles.main}>
       <div>Please login</div>
